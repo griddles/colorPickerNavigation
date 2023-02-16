87249cee06640d8ec4e57801df823407b22d6fc1
griddles <89864073+griddles@users.noreply.github.com>
2023-02-16 14:34:45 -0700

    mostly working tm

--- app/src/main/java/com/example/colorpickernavigation/MainActivity.kt
+++ app/src/main/java/com/example/colorpickernavigation/MainActivity.kt
@@ -13,18 +13,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.colorpickernavigation.databinding.ActivityMainBinding
+import com.example.colorpickernavigation.databinding.AppBarMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: AppBarMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //supportActionBar?.hide()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = AppBarMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
