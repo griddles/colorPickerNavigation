216c3594f2bc9258c12e48be6711ce59705421ef
griddles <89864073+griddles@users.noreply.github.com>
2023-02-23 12:23:15 -0700

    Revert "mostly working tm"
    This reverts commit 87249cee06640d8ec4e57801df823407b22d6fc1.

--- app/src/main/java/com/example/colorpickernavigation/MainActivity.kt
+++ app/src/main/java/com/example/colorpickernavigation/MainActivity.kt
@@ -13,18 +13,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.colorpickernavigation.databinding.AppBarMainBinding
+import com.example.colorpickernavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: AppBarMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //supportActionBar?.hide()
 
-        binding = AppBarMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
